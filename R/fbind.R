57a9c956e300dfd45283e923be6f2c43faa1695f
Oscar E. Aceves <acevesoscare@gmail.com>
2021-03-03 14:25:22 -0800

    Initial commit

--- R/fbind.R
+++ R/fbind.R
@@ -1,1 +1,5 @@
 "Factors are a very useful type of variable in R, but they can also be very aggravating. This package provides some helper functions for the care and feeding of factors."
+#'@export
+fbind <- function(a, b) {
+  factor(c(as.character(a), as.character(b)))
+}
